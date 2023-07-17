10fb397cff03ea8545b80ff70a1923cb4f9828e0
Nik <zuzichev@mail.ru>
2023-07-17 15:57:11 +0500

    тесты по 3 лекции

--- m1l3-oop/src/main/kotlin/Main.kt
+++ m1l3-oop/src/main/kotlin/Main.kt
@@ -1,18 +1,5 @@
 package ru.otus.otuskotlin.productproperty.m1l3
 
-import ru.otus.otuskotlin.productproperty.m1l3.MyClass.MyStatic.myAnyNumber
-
 fun main() {
     println("Hello, module M1L3!")
-    println("There is a static number: $myAnyNumber")
-}
-
-
-class MyClass{
-    companion object MyStatic {
-        const val myAnyNumber: Double = 0.0
-    }
-
-    var a: String = "aaaaa" // если декомпилить, то будут геттер+сеттер. У val только геттер
-
 }
